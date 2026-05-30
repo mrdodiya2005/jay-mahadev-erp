fb024d3aaccb1caab0368cdc1b57565fd7bb8a4a
mrdodiya2005 <kiwice1240@ixunbo.com>
2026-05-30 16:22:43 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Aa ekdam basic service worker che. 
+  // PWA na rules pass karva mate aatlu purtu che.
+});
